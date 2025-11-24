7006bfb47a2fd76dceb7df4be94e52e2b93867fa
lalz7 <izlalchaidar@upi.edu>
2025-11-24 16:48:02 +0700

    reset siswa

--- CBT_Sekolah/app.py
+++ CBT_Sekolah/app.py
@@ -84,4 +84,4 @@ with app.app_context():
         print("Admin default dibuat → username: admin | password: admin123")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
